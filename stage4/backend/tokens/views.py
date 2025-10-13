94984e7580ad03882435655a047ca86c1b0a9355
XavierLaforgue <xavierjimmys@gmail.com>
2025-10-13 22:26:29 +0200

    tokens create
    Add tokens app with its custom views and urls

---
+++ stage4/backend/tokens/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+    TokenBlacklistView
+)
+
+# Create your views here.
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    @swagger_auto_schema(tags=['Authentication'])
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+
+class CustomTokenRefreshView(TokenRefreshView):
+    @swagger_auto_schema(tags=['Authentication'])
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+
+class CustomTokenVerifyView(TokenVerifyView):
+    @swagger_auto_schema(tags=['Authentication'])
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+
+class CustomTokenBlacklistView(TokenBlacklistView):
+    @swagger_auto_schema(tags=['Authentication'])
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
