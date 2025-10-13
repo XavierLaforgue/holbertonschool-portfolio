186ac8e0de6c6c36e9132d77a654bd0793919754
XavierLaforgue <xavierjimmys@gmail.com>
2025-10-13 22:25:24 +0200

    accounts/views.py update
    Customize swagger documentation for user and profile views

--- stage4/backend/accounts/views.py
+++ stage4/backend/accounts/views.py
@@ -6,6 +6,7 @@ from .serializers import ProfileSerializer
 from django.contrib.auth.models import User
 from .serializers import UserCreateSerializer, UserReadSerializer
 from django.utils.crypto import get_random_string
+from drf_yasg.utils import swagger_auto_schema
 
 
 # Create your views here.
@@ -19,6 +20,14 @@ def index(request):
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = CustomUser.objects.all()
 
+    @swagger_auto_schema(tags=['Users'])
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Users'])
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
     def get_serializer_class(self):
         if self.request.method == 'POST':
             return UserCreateSerializer
@@ -28,6 +37,22 @@ class UserListCreateView(generics.ListCreateAPIView):
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserReadSerializer
+
+    @swagger_auto_schema(tags=['User ID'])
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['User ID'])
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['User ID'])
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['User ID'])
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -57,7 +82,31 @@ class ProfileListCreateView(generics.ListCreateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
+    @swagger_auto_schema(tags=['Profiles'])
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Profiles'])
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
 
 class ProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+    @swagger_auto_schema(tags=['Profile ID'])
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Profile ID'])
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Profile ID'])
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+
+    @swagger_auto_schema(tags=['Profile ID'])
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
