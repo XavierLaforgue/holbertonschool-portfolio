424ddbeb31c065bfd67b71c510b6e291952baef8
Xavier Laforgue <xavierjimmys@gmail.com>
2026-03-15 16:35:31 +0100

    Add profile property to CustomUser so profile is recognized as such by the static type checker

--- app/back-end/accounts/models.py
+++ app/back-end/accounts/models.py
@@ -51,6 +51,58 @@ class CustomUserManager(UserManager):
         )
 
 
+class Profile(UUIDModel):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE,
+                                null=False,
+                                blank=False)
+    birth_date = models.DateField(blank=True, null=True, default=None)
+    # TODO: make `display_name` nullable and let front-end replace other
+    # user's `display_name` for `unnamed` and own's `display_name` for `email`
+
+    @staticmethod
+    def default_display_name():
+        n = 1
+        while True:
+            display_name = f"unnamed_user_{n}"
+            if not Profile.objects.filter(display_name=display_name).first():
+                return display_name
+            n += 1
+    display_name = models.CharField(blank=False, null=False,
+                                    default=default_display_name,
+                                    max_length=150,
+                                    unique=True
+                                    )
+    bio = models.TextField(blank=True, null=True, default=None,
+                           max_length=500)
+    # TODO: add avatar image field, perhaps with:
+    # avatar = models.ImageField(
+    #     upload_to='avatars/',
+    #     default='avatars/default_avatar.png',
+    #     blank=True, null=True)
+    favorite_anime_custom = models.CharField(blank=True, null=True,
+                                             max_length=150)
+    # TODO: Create a relationship with an anime entity that is replaced by a
+    # simple text (the title of the anime it was related to) on delete
+    # favorite_anime = models.ForeignKey(
+    #   Anime,
+    #   on_delete=models.PROTECT,
+    #   related_name="favorites"
+    # )
+    favorite_meal = models.CharField(blank=True, null=True, max_length=150)
+    location = models.CharField(blank=True, null=True, max_length=150)
+    myAnimeList_profile = models.URLField(blank=True, null=True,
+                                          max_length=200)
+    dietary_preferences = models.TextField(blank=True, null=True,
+                                           max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    cleared_at = models.DateTimeField(blank=False, null=True, default=None)
+
+    def __str__(self) -> str:
+        return self.user.email
+
+
 class CustomUser(
                  UUIDModel,
                  AbstractUser,
@@ -92,60 +144,12 @@ class CustomUser(
                                           default=None)
     deleted_at = models.DateTimeField(blank=False, null=True, default=None)
 
+    @property
+    def profile(self) -> Profile:
+        return Profile.objects.get(user=self)
+
     def __str__(self) -> str:
         return self.email
-
-
-def default_display_name():
-    n = 1
-    while True:
-        display_name = f"unnamed_user_{n}"
-        if not Profile.objects.filter(display_name=display_name).first():
-            return display_name
-        n += 1
-
-
-class Profile(UUIDModel):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL,
-                                on_delete=models.CASCADE,
-                                null=False,
-                                blank=False)
-    birth_date = models.DateField(blank=True, null=True, default=None)
-    # TODO: make `display_name` nullable and let front-end replace other
-    # user's `display_name` for `unnamed` and own's `display_name` for `email`
-    display_name = models.CharField(blank=False, null=False,
-                                    default=default_display_name,
-                                    max_length=150,
-                                    unique=True
-                                    )
-    bio = models.TextField(blank=True, null=True, default=None,
-                           max_length=500)
-    # TODO: add avatar image field, perhaps with:
-    # avatar = models.ImageField(
-    #     upload_to='avatars/',
-    #     default='avatars/default_avatar.png',
-    #     blank=True, null=True)
-    favorite_anime_custom = models.CharField(blank=True, null=True,
-                                             max_length=150)
-    # TODO: Create a relationship with an anime entity that is replaced by a
-    # simple text (the title of the anime it was related to) on delete
-    # favorite_anime = models.ForeignKey(
-    #   Anime,
-    #   on_delete=models.PROTECT,
-    #   related_name="favorites"
-    # )
-    favorite_meal = models.CharField(blank=True, null=True, max_length=150)
-    location = models.CharField(blank=True, null=True, max_length=150)
-    myAnimeList_profile = models.URLField(blank=True, null=True,
-                                          max_length=200)
-    dietary_preferences = models.TextField(blank=True, null=True,
-                                           max_length=150)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    cleared_at = models.DateTimeField(blank=False, null=True, default=None)
-
-    def __str__(self) -> str:
-        return self.user.email
 
 
 # receive signals of events of `CustomUser` instance being saved and create a
