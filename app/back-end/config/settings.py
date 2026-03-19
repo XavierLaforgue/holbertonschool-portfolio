ec106cbb58e1a55636a4b76d38f6b4371755a68d
Xavier Laforgue <xavierjimmys@gmail.com>
2026-03-19 13:30:23 +0100

    Set directory for django static files so they are collected by django to be served by Nginx

--- app/back-end/config/settings.py
+++ app/back-end/config/settings.py
@@ -237,6 +237,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# Directory where `collectstatic` gathers all static files for production.
+# Nginx serves this directory directly instead of Django.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (user-uploaded content)
 # https://docs.djangoproject.com/en/6.0/topics/files/
